a784421dd6fef31f695861df5494cec536d03ddf
ngAtesz <attila.molnar@codecool.com>
2016-03-21 17:18:35 +0100

    Employee class created

---
+++ Week13A/Week13A/Employee.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Week13A
+{
+    class Employee : Person
+    {
+        private int salary;
+
+        public int Salary
+        {
+            get
+            {
+                return salary;
+            }
+
+            set
+            {
+                salary = value;
+            }
+        }
+
+        private string profession;
+
+        public string Profession
+        {
+            get
+            {
+                return profession;
+            }
+
+            set
+            {
+                profession = value;
+            }
+        }
+
+        public Room Room;
+
+        public Employee()
+        {
+            Room = null;
+        }
+
+        public Employee(string name, DateTime birthDate, int salary, string profession) : base(name, birthDate)
+        {
+            this.salary = salary;
+            this.profession = profession;
+            Room = null;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + String.Format(", salary: {0}, profession: {1}, room: {2}", salary, profession, Room.Number);
+        }
+    }
+
+    class Room
+    {
+        private int number;
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+
+            set
+            {
+                number = value;
+            }
+        }
+
+        public Room(int number)
+        {
+            this.number = number;
+        }
+    }
+}
--- Week13A/Week13A/Program.cs
+++ Week13A/Week13A/Program.cs
@@ -12,9 +12,13 @@ namespace Week13A
         {
             Person jozsi = new Person("Boda Jozsi", new DateTime(1975, 1, 1));
             Person pakko = new Person("Monoczki Pakko", new DateTime(1980, 1, 15));
+            Employee atesz = new Employee("Molnar Atesz", new DateTime(1989, 5, 6), 1000000, "Software engineer");
+            atesz.Room = new Room(111);
+            atesz.Room.Number = 1;
 
             Console.WriteLine(jozsi);
             Console.WriteLine(pakko);
+            Console.WriteLine(atesz);
             Console.ReadKey();
         }
     }
