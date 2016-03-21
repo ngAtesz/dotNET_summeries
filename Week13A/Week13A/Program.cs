13a838a4759be0d97a10d016bc023762b4044001
ngAtesz <attila.molnar@codecool.com>
2016-03-21 16:56:15 +0100

    Test code for Persons added

--- Week13A/Week13A/Program.cs
+++ Week13A/Week13A/Program.cs
@@ -10,6 +10,12 @@ namespace Week13A
     {
         static void Main(string[] args)
         {
+            Person jozsi = new Person("Boda Jozsi", new DateTime(1975, 1, 1));
+            Person pakko = new Person("Monoczki Pakko", new DateTime(1980, 1, 15));
+
+            Console.WriteLine(jozsi);
+            Console.WriteLine(pakko);
+            Console.ReadKey();
         }
     }
 }
