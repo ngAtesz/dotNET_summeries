56fe374fdbefaa65f20c20215a483e9104e067eb
ngAtesz <attila.molnar@codecool.com>
2016-03-21 17:41:11 +0100

    SortString application added

---
+++ Week13A/Week13A_3_SortString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week13A_3_SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] sa = s.Split(' ');
+
+            Array.Sort(sa);
+
+            s = string.Join(" ", sa);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
