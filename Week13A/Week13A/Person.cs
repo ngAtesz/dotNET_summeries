b82bbc46926b7dc02a38d7a0d2e0751a891bc348
molnar45 <molnar45@hotmail.com>
2016-03-21 15:16:31 +0100

    Week 13A and its Person class added

---
+++ Week13A/Week13A/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Week13A
+{
+    class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        private DateTime birthDate;
+
+        public DateTime BirthDate
+        {
+            get
+            {
+                return birthDate;
+            }
+
+            set
+            {
+                birthDate = value;
+            }
+        }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, DateTime birthDate)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+        }
+
+        public enum Genders : int { Male, Female };
+
+        public Genders gender;
+
+        public override string ToString()
+        {
+            return String.Format("name: {0}, birthdate: {1}", this.name, this.birthDate);
+        }
+    }
+}
