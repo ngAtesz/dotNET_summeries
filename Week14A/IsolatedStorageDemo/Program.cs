a4d35d5acf67dd8128fb511282b29c4fec86b752
ngAtesz <attila.molnar@codecool.com>
2016-03-22 14:59:12 +0100

    IsolatedStorage file read added to IsolatedStorageDemo

--- Week14A/IsolatedStorageDemo/Program.cs
+++ Week14A/IsolatedStorageDemo/Program.cs
@@ -9,11 +9,27 @@ namespace IsolatedStorageDemo
         static void Main(string[] args)
         {
             IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
-            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            IsolatedStorageFileStream userStream;
 
-            StreamWriter userWriter = new StreamWriter(userStream);
-            userWriter.WriteLine("User Prefs");
-            userWriter.Close();
+            string userSettingsFileName = "UserSettings.set";
+            string[] files = userStore.GetFileNames(userSettingsFileName);
+            if (files.Length == 0)
+            {
+                userStream = new IsolatedStorageFileStream(userSettingsFileName, FileMode.Create, userStore);
+                StreamWriter userWriter = new StreamWriter(userStream);
+                userWriter.WriteLine("User Prefs");
+                userWriter.Close();
+                Console.WriteLine("{0} created.", userSettingsFileName);
+            }
+            else
+            {
+                userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+                StreamReader userReader = new StreamReader(userStream);
+                string contents = userReader.ReadToEnd();
+                Console.WriteLine("IsolatedStorageFile content: {0}", contents);
+            }
+            
+            Console.Read();
         }
     }
 }
