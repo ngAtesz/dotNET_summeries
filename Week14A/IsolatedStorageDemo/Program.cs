dbc0ddfb3399ced665f6f4f8565ad53b463b20a7
ngAtesz <attila.molnar@codecool.com>
2016-03-22 14:52:51 +0100

    IsolatedStorageDemo added to Week14A

---
+++ Week14A/IsolatedStorageDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace IsolatedStorageDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+        }
+    }
+}
